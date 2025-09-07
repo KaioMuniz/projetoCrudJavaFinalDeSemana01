7b83023de7381b3f8e46e6b8624b9ef91835bc97
Kaio Muniz <kkaioribeiro@gmail.com>
2025-09-06 23:18:19 -0300

    projeto iniciado com devtools e spring web

---
+++ src/main/java/br/com/kaiomuniz/ProjetoCrudJavaFinalDeSemana01Application.java
@@ -0,0 +1,13 @@
+package br.com.kaiomuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudJavaFinalDeSemana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudJavaFinalDeSemana01Application.class, args);
+	}
+
+}
